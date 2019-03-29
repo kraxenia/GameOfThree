028ae78dae8274dfac65b0cd66a7357136846a35
xenia <xenia_k@optimove.com>
2019-03-29 20:45:20 +0300

    remove wS for test

--- server.js
+++ server.js
@@ -16,36 +16,36 @@ app.get('/*', function(req,res) {
 app.listen(process.env.PORT || 3000);
 
 //initialize a simple http server
-const server = http.createServer(app);
+//const server = http.createServer(app);
 
-//initialize the WebSocket server instance
-const ws = new wss.Server({ server });
-let CLIENTS={};
-const webSocket = new wss.Server({ port:  8080 });
+// //initialize the WebSocket server instance
+// const ws = new wss.Server({ server });
+// let CLIENTS={};
+// const webSocket = new wss.Server({ port:  8080 });
 
-webSocket.on('connection', function connection(ws) {
-  ws.id = uuid.v4();
-  CLIENTS[ws.id] = ws;
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-    let parsedMsg = JSON.parse(message);
-    if (!CLIENTS[parsedMsg.recipient]) {
-      let errorMsg = {
-        message: 'Client Is Not Available. Please try another one.',
-        type: 'isError',
-        recipient: parsedMsg.author
-      }
+// webSocket.on('connection', function connection(ws) {
+//   ws.id = uuid.v4();
+//   CLIENTS[ws.id] = ws;
+//   ws.on('message', function incoming(message) {
+//     console.log('received: %s', message);
+//     let parsedMsg = JSON.parse(message);
+//     if (!CLIENTS[parsedMsg.recipient]) {
+//       let errorMsg = {
+//         message: 'Client Is Not Available. Please try another one.',
+//         type: 'isError',
+//         recipient: parsedMsg.author
+//       }
 
-      CLIENTS[parsedMsg.author].send(JSON.stringify(errorMsg));
-      return;
-    }
+//       CLIENTS[parsedMsg.author].send(JSON.stringify(errorMsg));
+//       return;
+//     }
 
-    ws.on('close', req => {
-      delete CLIENTS[req.id];
-    });
+//     ws.on('close', req => {
+//       delete CLIENTS[req.id];
+//     });
 
-    CLIENTS[parsedMsg.recipient].send(message);
-  });
+//     CLIENTS[parsedMsg.recipient].send(message);
+//   });
 
-  ws.send(JSON.stringify({recipient: ws.id}));
-});
+//   ws.send(JSON.stringify({recipient: ws.id}));
+// });
